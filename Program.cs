3b91f6018f60f1a12415ffa18b1ac69c289be60d
Gert Drapers <1533850+gertd@users.noreply.github.com>
2025-03-21 10:22:45 -0700

    TopazClient

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Aserto.Directory.Reader.V3;
+using Aserto.Directory.Common.V3;
+
+
+var handler = new HttpClientHandler();
+handler.ServerCertificateCustomValidationCallback = 
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:9292", 
+  new GrpcChannelOptions { HttpHandler = handler});
+
+var client = new Reader.ReaderClient(channel);
+
+var resp = await client.GetObjectsAsync(new GetObjectsRequest{ObjectType = "user", Page = new PaginationRequest{Size = 10}});
+
+foreach (var r in resp.Results) {
+  Console.WriteLine($"type: {r.Type} id: {r.Id}");
+}
